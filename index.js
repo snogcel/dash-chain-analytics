7fce7d299daf463d30aa42caed40a5c76eca9061
snogcel <ledniknoj@gmail.com>
2018-10-26 15:28:52 -0600

    refactor transaction fetcher

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 var blockExplorer = require('./lib/blockExplorer.js');
+var DataFetcher = require('./lib/fetcher.js');
+
 var database = require('./lib/database.js');
 var async = require('async');
 
@@ -10,101 +12,10 @@ program
     .version('0.1.0')
     .option('-s, --start <n>', 'start scanning at given block height', parseInt)
     .option('-e, --end <n>', 'stop scanning at given block height', parseInt)
+    .option('-t, --test', 'test mode')
     .parse(process.argv);
 
 var start, end;
-var blockStack = [];
-var txStack = [];
-
-// TODO - move to proper function
-
-var getTxs = function(hash, done) {
-    console.log("> fetching tx " + hash);
-
-    setTimeout(
-        function() {
-
-            blockExplorer.getTx(hash, function(err, res) {
-
-                var privateSend = false;
-
-                if (typeof res !== 'undefined' && res) {
-
-                    // if TX has > 1 input and 0 fee we assume it's privateSend
-                    if (res.vin.length > 1 && res.fees === 0) {
-                        privateSend = true;
-                    }
-
-                    var data = {
-                        height: res.blockheight,
-                        txhash: res.txid,
-                        inputs: res.vin.length,
-                        fee: res.fees,
-                        txlock: res.txlock,
-                        privatesend: privateSend
-                    };
-
-                    if (res.vin.length > 1) { // ignore mining transaction-only blocks
-
-                        txStack.push(data);
-
-                    }
-
-                }
-
-                done();
-
-
-            });
-
-        }, Math.floor((Math.random() * 2000) + 1000));
-};
-
-var getBlocks = function(start, done) {
-    console.log("> fetching block " + start);
-    setTimeout(
-        function() {
-
-            blockExplorer.getBlockIndex(start, function(err, res) {
-
-                blockExplorer.getBlock(res.blockHash, function(err, res) {
-
-                    for (var i = 0; i < res.tx.length; i++) {
-
-                        txQueue.push(res.tx[i]); // pull TXs from block
-
-                    }
-
-                    var data = {
-                        height: res.height,
-                        difficulty: res.difficulty,
-                        time: res.time
-                    };
-
-                    blockStack.push(data);
-
-                    done();
-
-                });
-
-            });
-
-        }, Math.floor((Math.random() * 2000) + 1000));
-};
-
-var blockQueue = async.queue(getBlocks, 5);
-blockQueue.drain = function() {
-    database.add('block', blockStack, function(err, res) {
-        console.log(res);
-    });
-};
-
-var txQueue = async.queue(getTxs, 5);
-txQueue.drain = function() {
-    database.add('transaction', txStack, function(err, res) {
-        console.log(res);
-    });
-};
 
 if (program.start && program.end) {
     start = program.start;
@@ -112,10 +23,22 @@ if (program.start && program.end) {
 
     for (start; start < end; start++) {
 
-        blockQueue.push(start);
+        DataFetcher.blockQueue.push(start);
 
     }
 
 } else {
-    console.log("Error: please provide start and end parameter.")
+
+    if (program.test) {
+
+        console.log('-- test mode --');
+
+        DataFetcher.blockQueue.push(700007);
+
+    } else {
+
+        console.log("Error: please provide start and end parameter.")
+
+    }
+
 }
